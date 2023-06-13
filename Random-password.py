54cf5fa9a3aa7f97a0a1222146585d894aecebca
kohandaniamir <amirkohandani124498@gmail.com>
2023-06-13 16:15:39 -0700

    Random_password

---
+++ Random-password.py
@@ -0,0 +1,17 @@
+import random
+
+lower = "abcdefghijklmnopqrstuvwxyz"
+
+upper= lower.upper()
+
+numbers = "1234567890"
+
+symbol = "!@#$%^&*()[];'0"
+
+length = 15
+
+string = lower + upper + numbers +symbol
+
+password = "".join(random.sample(string,length))
+
+print(f"your password is : {password}")
